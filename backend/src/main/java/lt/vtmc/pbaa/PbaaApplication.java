e79f39f41122934363b84c5bb815adb23e2518c2
Julius <jzabulenas@protonmail.com>
2022-04-14 16:12:26 +0300

    add backend and frontend base folders with required files, as well as .gitignore.

---
+++ backend/src/main/java/lt/vtmc/pbaa/PbaaApplication.java
@@ -0,0 +1,13 @@
+package lt.vtmc.pbaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbaaApplication.class, args);
+	}
+
+}
