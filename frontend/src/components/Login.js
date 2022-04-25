6acaeb44f76faf4b271b88e8354406b1a7e9ee8c
Julius <jzabulenas@protonmail.com>
2022-04-25 21:22:11 +0300

    add 1em marginTop to register and login forms

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -22,7 +22,7 @@ export default function Login() {
             <div className="container h-100">
                 <div className="row d-flex justify-content-center align-items-center h-5">
                     <div className="col-xl-12 col-lg-11">
-                        <div className="card text-black" style={{ borderRadius: "25px", marginTop: "0px"}}>
+                        <div className="card text-black" style={{ borderRadius: "25px", marginTop: "1em" }}>
                             <div className="card-body p-md-5">
                                 <div className="row justify-content-center">
                                     <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center">
