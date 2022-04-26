73244e09581a497b53e5052aab416f6f05f6f9e7
Julius <jzabulenas@protonmail.com>
2022-04-26 15:43:50 +0300

    change Income inputs to work with a form that uses React Form Hook

--- frontend/src/components/Income.js
+++ frontend/src/components/Income.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Income.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import AuthService from "../services/auth.service"
+import { useForm } from "react-hook-form";
 
 // This code copypasted from: https://codepen.io/fido123/pen/xzvxNw
 // JavaScript is not included in this code, only html and css
 
 export default function Income() {
-    const [incomeName, setIncomeName] = useState("")
-    const [amount, setAmount] = useState("")
+    const [allIncome, setAllIncome] = useState([])
+    const [forceRender, setForceRender] = useState(false)
     const currentUser = AuthService.getCurrentUser();
+    const { register, handleSubmit, formState: { errors } } = useForm({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
 
-    const addIncome = async () => {
+    // Add user's income to database from the inputs
+    const onSubmit = async (data) => {
         const response = await fetch(
             "http://localhost:8080/api/income/",
             {
@@ -21,13 +24,68 @@ export default function Income() {
                     'Authorization': `Bearer ${currentUser.accessToken}`
                 },
                 body: JSON.stringify({
-                    "incomeName": incomeName,
-                    "date": "2021-04-25",
-                    "amount": amount
+                    "incomeName": data.incomeName,
+                    "date": data.date,
+                    "amount": data.amount
                 })
             }
         )
+
+        setForceRender(!forceRender)
     }
+
+    const editIncome = async (income) => {
+        const response = await fetch(
+            "http://localhost:8080/api/income/",
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${currentUser.accessToken}`
+                },
+                body: JSON.stringify({
+                    "incomeName": null,
+                    "date": null,
+                    "amount": null
+                })
+            }
+        )
+
+        setForceRender(!forceRender)
+    }
+
+    const removeIncome = async (id) => {
+        const response = await fetch(
+            `http://localhost:8080/api/income/${id}`,
+            {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${currentUser.accessToken}`
+                }
+            }
+        )
+
+        setForceRender(!forceRender)
+    }
+
+    // Fetch all user's income from database to display down below
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`http://localhost:8080/api/income/user/${currentUser.id}`,
+                {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${currentUser.accessToken}`
+                    }
+                });
+            const data = await response.json();
+            setAllIncome(data);
+        }
+
+        fetchData();
+    }, [forceRender]);
 
     return (
         <>
@@ -37,7 +95,8 @@ export default function Income() {
                         <div className="budget">
                             <div className="budget__title">
                                 <h1 className="display-4 pt-3">
-                                    <span className="budget__title-month">%Month%</span> mėnesio pajamos
+                                    {/* <span className="budget__title-month">%Month%</span> mėnesio pajamos */}
+                                    <span className="budget__title-month"></span> Pajamos
                                 </h1>
                                 <div className="mt-3 mb-5 budget__value">+ 2,345.66</div>
 
@@ -56,33 +115,42 @@ export default function Income() {
                 </div>
             </div>
 
-
             <div className="bottom mt-3">
                 <div className="container">
                     <div className="add">
                         <div className="row text-center add__container">
-                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 input-group my-3">
 
-                                <input type="text"
+                            <form onSubmit={handleSubmit(onSubmit)} className="col-12 col-sm-6 col-md-6 col-lg-6 input-group my-3">
+                                <input
+                                    {...register("incomeName", { required: true, minLength: 4 })}
+                                    type="text"
                                     className="form-control add__description"
                                     placeholder="Aprašymas"
-                                    onChange={e => setIncomeName(e.target.value)}
                                 />
-                                <input type="date" className="form-control add__date" placeholder="Date" />
+                                {errors?.incomeName?.type === "required" && <p>Šis laukas yra privalomas</p>}
+                                {errors?.incomeName?.type === "minLength" && <p>Aprašymas turi būti bent 4 simbolių ilgio</p>}
 
-                                <input type="number"
+                                <input
+                                    {...register("date", { required: true })}
+                                    type="date"
+                                    className="form-control add__date"
+                                    placeholder="Data"
+                                />
+
+                                <input
+                                    {...register("amount", { required: true, min: 0.01 })}
+                                    type="number"
                                     className="form-control add__value"
                                     placeholder="Kiekis"
-                                    onChange={e => setAmount(e.target.value)}
                                 />
 
                                 <div className="input-group-append">
-                                    <button onClick={() => addIncome()} className="btn" type="button">
+                                    <button className="btn" type="submit">
                                         <FontAwesomeIcon icon="circle-check" className='add__btn' />
                                     </button>
                                 </div>
+                            </form>
 
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -92,6 +160,19 @@ export default function Income() {
                         <div className="col-12 income">
                             <h2 className="income__title">Pajamos</h2>
                             <div className="container income__list"></div>
+
+                            {/* Display user's income on the page */}
+                            {allIncome.map(income => {
+                                return (
+                                    <p>
+                                        {income.incomeName}&nbsp;
+                                        {income.date}&nbsp;
+                                        {income.amount}&euro;&nbsp;
+                                        <button>Redaguoti</button>&nbsp;
+                                        <button onClick={() => removeIncome(income.id)}>Pašalinti</button>
+                                    </p>
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
