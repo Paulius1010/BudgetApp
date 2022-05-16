c61b1c3994ed85449e067ae81a4ee3b77a2a20ab
Julius <jzabulenas@protonmail.com>
2022-05-16 22:39:55 +0300

    comment out existing code in HomeLoggedIn component, and replace it with a div tag and container className

--- frontend/src/components/HomeLoggedIn.js
+++ frontend/src/components/HomeLoggedIn.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React from 'react';
 import AuthService from "../services/auth.service";
-import Header from './Header'
-import NavbarAna from './NavbarAna'
-import SideBar from './SideBar'
+import Header from './Header';
+import NavbarAna from './NavbarAna';
+import SideBar from './SideBar';
 
 export default function HomeLoggedIn() {
     const currentUser = AuthService.getCurrentUser();
 
     return (
-        <>
-            <div>
-                <h2>Sveiki {currentUser.email}</h2>
-            </div>
+        // <>
+        //     <div>
+        //         <h2>Sveiki {currentUser.email}</h2>
+        //     </div>
 
-            {/* <div>
-                <Header />
-            </div> */}
-            <div className='row'>
-                <div className='row col-8 ps-4'>
-                    <div className='col-12 mt-4'><NavbarAna /></div>
-                    <h2>Some content</h2>
-                </div>
-                <div className='col-4'>
-                    <SideBar />
-                </div>
-            </div>
-        </>
-    )
+        //     {/* <div>
+        //         <Header />
+        //     </div> */}
+        //     <div className='row'>
+        //         <div className='row col-8 ps-4'>
+        //             <div className='col-12 mt-4'><NavbarAna /></div>
+        //             <h2>Some content</h2>
+        //         </div>
+        //         <div className='col-4'>
+        //             <SideBar />
+        //         </div>
+        //     </div>
+        // </>
+
+        <div className="container">
+            <h3>Hello</h3>
+        </div>
+    );
 }
