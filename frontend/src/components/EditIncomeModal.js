fd8ccbaf0e98315ceee347213de5607e43d2c581
Julius <jzabulenas@protonmail.com>
2022-05-17 23:09:26 +0300

    fix where EditIncomeModal would 'save' even if there was errors in validation

--- frontend/src/components/EditIncomeModal.js
+++ frontend/src/components/EditIncomeModal.js
@@ -4,6 +4,7 @@ import AuthService from "../services/auth.service";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as bootstrap from 'bootstrap';
 
 export default function EditIncomeModal({ id, incomeName, date, amount, forceRender, setForceRender }) {
     const currentUser = AuthService.getCurrentUser();
@@ -15,6 +16,12 @@ export default function EditIncomeModal({ id, incomeName, date, amount, forceRen
     const mm = String(today.getMonth() + 1).padStart(2, '0');
     const yyyy = today.getFullYear();
     today = yyyy + '-' + mm + '-' + dd;
+
+    const hideModal = () => {
+        const myModalEl = document.getElementById('id' + id);
+        const modal = bootstrap.Modal.getInstance(myModalEl);
+        modal.hide();
+    };
 
     const onSubmit = async (data) => {
         const response = await fetch(
@@ -36,6 +43,7 @@ export default function EditIncomeModal({ id, incomeName, date, amount, forceRen
 
         if (response.status === 200) {
             successMessage();
+            hideModal();
         }
         else {
             (errorMessage('Klaida!'));
@@ -78,7 +86,7 @@ export default function EditIncomeModal({ id, incomeName, date, amount, forceRen
             </button>
 
             <div
-                className="modal fade"
+                className="modal"
                 id={"id" + id}
                 data-bs-backdrop="static"
                 data-bs-keyboard="false"
@@ -161,7 +169,7 @@ export default function EditIncomeModal({ id, incomeName, date, amount, forceRen
                                 <button
                                     type="submit"
                                     className="btn btn-primary"
-                                    data-bs-dismiss="modal"
+                                // data-bs-dismiss="modal"
                                 >
                                     Išsaugoti
                                 </button>
