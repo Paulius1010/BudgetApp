598c078731ca65fa58ca21a9175c74219d0785b9
Ana <pvz@mail.com>
2022-05-19 12:21:49 +0300

    changed up statistics chart

--- frontend/src/components/HomeLoggedIn.js
+++ frontend/src/components/HomeLoggedIn.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import AuthService from "../services/auth.service";
-// import Header from './Header';
-// import SideBar from './SideBar';
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
@@ -9,18 +7,18 @@ export default function HomeLoggedIn() {
     const currentUser = AuthService.getCurrentUser();
 
     const [income, setIncome] = useState([]);
-    const [expense, setExpense] = useState([]); //mine
+    const [expense, setExpense] = useState([]);
 
     const chartIncomeAmount = income.map(x => x.amount);
     const chartIncomeNames = income.map(x => x.incomeName);
-    const chartExpenseAmount = expense.map(x => x.amount); //mine
-    const chartExpenseNames = expense.map(x => x.expenseName); //mine
+    const chartExpenseAmount = expense.map(x => x.amount);
+    const chartExpenseNames = expense.map(x => x.expenseName);
 
     const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
     const chartIncomeColors = [];
     const chartIncomeColorsBorder = [];
-    const chartExpenseColors = []; //mine
-    const chartExpenseColorsBorder = []; //mine
+    const chartExpenseColors = [];
+    const chartExpenseColorsBorder = [];
 
     // Generates random RGB values for the displayed incomes
     for (let i = 1; i <= income.length; i++) {
@@ -32,8 +30,8 @@ export default function HomeLoggedIn() {
 
         chartIncomeColors.push(rgb);
         chartIncomeColorsBorder.push(rgbBorder);
-        chartExpenseColors.push(rgb); //mine
-        chartExpenseColorsBorder.push(rgbBorder); //mine
+        chartExpenseColors.push(rgb);
+        chartExpenseColorsBorder.push(rgbBorder);
     }
 
     // Fetch all user's income from database to display down below
@@ -90,7 +88,7 @@ export default function HomeLoggedIn() {
         };
 
         fetchData();
-    }, []) //mine
+    }, [])
 
 
     ChartJS.register(ArcElement, Tooltip, Legend);
@@ -107,7 +105,7 @@ export default function HomeLoggedIn() {
         ],
     };
 
-    ChartJS.register(ArcElement, Tooltip, Legend); //mine
+    ChartJS.register(ArcElement, Tooltip, Legend);
     const expenseData = {
         labels: chartExpenseNames,
         datasets: [
@@ -119,55 +117,67 @@ export default function HomeLoggedIn() {
                 borderWidth: 1,
             },
         ],
-    }; //mine
+    };
 
     return (
-        // <>
-        //     <div>
-        //         <h2>Sveiki {currentUser.email}</h2>
-        //     </div>
-
-        //     {/* <div>
-        //         <Header />
-        //     </div> */}
-        //     <div className='row'>
-        //         <div className='row col-8 ps-4'>
-        //             <div className='col-12 mt-4'><NavbarAna /></div>
-        //             <h2>Some content</h2>
-        //         </div>
-        //         <div className='col-4'>
-        //             <SideBar />
-        //         </div>
-        //     </div>
-        // </>
-
-        <div className="container">
-            <div className="row">
-                <div className='col-6'>
-                    <p>Šio mėnesio pajamos:</p>
-                    <div>
-                        <Doughnut
-                            data={incomeData}
-                            width={400}
-                            height={400}
-                            options={{ maintainAspectRatio: false }}
-                        />
-                    </div>
-                </div>
-
-                <div className='col-6'>
-                    <p>Šio mėnesio išlaidos:</p>
-                    <div>
-                        <Doughnut
-                            data={expenseData}
-                            width={400}
-                            height={400}
-                            options={{ maintainAspectRatio: false }}
-                        />
-                    </div>
-                </div>
-
+        <>
+            <div className='row'>
+                <h2>Sveiki {currentUser.email}</h2>
             </div>
-        </div>
+
+            <div className='row'>
+                <div className='row col-8 ps-4 mt-4'>
+                    <div className="row mb-4" style={{ margin: "0" }}>
+                        <button type="button" className="btn btn-success col-3 mx-3">IŠLAIDOS</button>
+                        <button type="button" className="btn btn-success col-3 mx-3">PAJAMOS</button>
+                        <button type="button" className="btn btn-success col-3 mx-3">BENDRA STAT.</button>
+                    </div>
+                </div>
+                <div className='col-4'>
+                    <div style={{ borderLeft: "solid 1px #696969" }}>
+                        <div className="row mt-3">
+                            <div className="col-2">
+                                <img src="https://via.placeholder.com/60x60?text=$" alt="placeholder" className="img-config"></img>
+                            </div>
+                            <div className="col-3">
+                                <h4>Likutis</h4>
+                                <h3>$YYYYYYY,00</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr></hr>
+
+
+            <div className="container">
+                <div className="row">
+                    <div className='col-6'>
+                        <p>Šio mėnesio pajamos:</p>
+                        <div>
+                            <Doughnut
+                                data={incomeData}
+                                width={400}
+                                height={400}
+                                options={{ maintainAspectRatio: false }}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='col-6'>
+                        <p>Šio mėnesio išlaidos:</p>
+                        <div>
+                            <Doughnut
+                                data={expenseData}
+                                width={400}
+                                height={400}
+                                options={{ maintainAspectRatio: false }}
+                            />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
     );
 }
